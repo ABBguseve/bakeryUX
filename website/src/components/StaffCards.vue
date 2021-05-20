30387fbcec693aa4bf7e2cc78139f5964d10e6fc
abbgorgar <124578Gd>
2021-05-20 11:06:03 +0200

    slider

--- website/src/components/StaffCards.vue
+++ website/src/components/StaffCards.vue
@@ -1,36 +1,40 @@
 <template>
   <div>
-    <div>
+    <div style="height:100%">
         <carousel-3d>
-            <slide v-for="member in coworkers.data" :key="member.id" :index="member.id">
-                <v-card
-                    class="my-2 mx-3"
-                    max-width="340"
-                    outlined
-                >
-                    <v-list-item three-line>
-                    <v-list-item-content>
-                        <div class="overline mb-4">
-                        {{ member.role}}
-                        </div>
-                        <v-list-item-title class="headline mb-1">
-                        {{ member.name}}
-                        </v-list-item-title>
-                        <v-list-item-subtitle>{{ member.email}}</v-list-item-subtitle>
-                        <v-list-item-subtitle>{{ member.phone}}</v-list-item-subtitle>
-                    </v-list-item-content>
-
-                    <v-list-item-avatar
-                        size="80"
+            <slide v-for="member in coworkers.data" :key="member.id" :index="member.id" style = "background: white; border: none">
+                <template slot-scope="{index, isCurrent, leftIndex, rightIndex}">
+                    <v-card
+                        :data-index="index"
+                        :class = "{ current: isCurrent, onLeft: (leftIndex >= 0), onRight: (rightIndex >= 0) }" 
+                        style = "position: relative; width: 100%; height: 100%;"
+                        outlined
                     >
-                    <v-img
-                        max-height="100"
-                        max-width="100"
-                        :src="member.avatar"
-                    ></v-img>
-                    </v-list-item-avatar>
-                    </v-list-item>
-                </v-card>
+                        <v-list-item three-line>
+                        <v-list-item-content >
+                            <div class="overline mb-4">
+                            {{ member.role}}
+                            </div>
+                            <v-spacer></v-spacer>
+                            <v-list-item-title class="headline mb-1">
+                            {{ member.name}}
+                            </v-list-item-title>
+                            <v-list-item-subtitle>{{ member.email}}</v-list-item-subtitle>
+                            <v-list-item-subtitle>{{ member.phone}}</v-list-item-subtitle>
+                        </v-list-item-content>
+
+                        <v-list-item-avatar
+                            size="80"
+                        >
+                        <v-img
+                            max-height="100"
+                            max-width="100"
+                            :src="member.avatar"
+                        ></v-img>
+                        </v-list-item-avatar>
+                        </v-list-item>
+                    </v-card>
+                </template>
             </slide>
         </carousel-3d>    
     </div>
