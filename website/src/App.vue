23169d3e74d127e5a05e45a8ba2a14329ab08d40
abbgorgar <124578Gd>
2021-05-28 11:53:38 +0200

    kljl

--- website/src/App.vue
+++ website/src/App.vue
@@ -40,9 +40,20 @@
         color="primary"
         dark
         @click="dialog = !dialog"
+        v-if="!currentUser"
       >
         Logga in
       </v-btn>
+
+      <v-avatar
+        class="mx-2"
+        v-else
+        clickable
+        @click="dialog = !dialog"
+      >
+        <img :src="currentUser.avatar">
+      </v-avatar>
+
     </v-toolbar>
     <v-main>
       <router-view />
@@ -60,24 +71,26 @@
             class="px-12 fontStyle_1"
             label="E-post"
             hide-details="auto"
+            v-model="mail"
           ></v-text-field>
           <v-spacer></v-spacer>
           <v-text-field
+            type="password"
             class="px-12 fontStyle_1"
             label="Lösenord"
             hide-details="auto"
+            v-model="password"
           ></v-text-field>
-          <v-divider></v-divider>
           <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn
-              class="navBtn ml-2"
+              class="navBtn my-3 mr-8"
               rounded
               depressed
               small
               color="primary"
               dark
-              @click="dialog = false"
+              @click="logIn"
             >
               Logga in
             </v-btn>
@@ -89,12 +102,49 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: "App",
 
   data: () => ({
-    dialog: false
+    dialog: false,
+    customers: {},
+    currentUser: null,
+    mail: '',
+    password: ''
   }),
+  mounted () {
+    axios
+      .get('http://localhost:3000/customers')
+      .then(response => (this.customers = response.data))
+  },
+  methods: {
+    logIn () {
+      let customers = this.customers
+      console.log("customers")
+      console.log(this.customers)
+      console.log("currentuser")
+      console.log(this.mail)
+      console.log(this.password)
+      // for (i = 0; i < this.customers.length; i++) {
+      //   console.log(this.customers[i])
+      // }
+      for (let c in customers) {
+        if (this.mail == customers[c].email) {
+          if (this.password == customers[c].password) {
+            this.currentUser = customers[c]
+            console.log(customers[c].name)
+            console.log("logged in")
+          }
+          else {
+            console.error("no matching password")
+          }
+        }
+      }
+      this.dialog = false
+    }
+  }
 };
 </script>
 
--- website/src/views/Info.vue
+++ website/src/views/Info.vue
@@ -12,6 +12,12 @@
     >
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur volutpat orci lectus, id scelerisque tortor fringilla in. Morbi nec tortor tristique, elementum leo eu, commodo ante. Morbi ornare quam sit amet egestas suscipit. Etiam suscipit in erat vel rhoncus. Nam sollicitudin ante neque, ac maximus leo aliquet eget. Donec nec aliquet lacus. Cras nec congue neque, tincidunt aliquet odio. Sed rhoncus quam eu urna finibus, vitae sollicitudin dolor convallis. Proin nulla lorem, accumsan non massa ac, aliquam ornare lorem. Maecenas ac lacus porttitor, euismod mi a, aliquet mi. Fusce et tincidunt sapien. Morbi at nisi dignissim, consectetur tellus vel, malesuada ligula. Sed arcu ex, posuere a mi et, maximus tincidunt mauris. Donec bibendum massa id orci auctor, sed lacinia ligula ornare. Vestibulum metus diam, iaculis sit amet euismod non, blandit eget mi.
     </h4>
+    <h4
+      style="font-family: 'Poppins', sans-serif"
+      class="my-5 px-15"
+    >
+      Curabitur volutpat orci lectus, id scelerisque tortor fringilla in. Morbi nec tortor tristique, elementum leo eu, commodo ante. Morbi ornare quam sit amet egestas suscipit. Etiam suscipit in erat vel rhoncus. Nam sollicitudin ante neque, ac maximus leo aliquet eget. Donec nec aliquet lacus. Cras nec congue neque, tincidunt aliquet odio. Sed rhoncus quam eu urna finibus, vitae sollicitudin dolor convallis. Proin nulla lorem, accumsan non massa ac, aliquam ornare lorem. Maecenas ac lacus porttitor, euismod mi a, aliquet mi. Fusce et tincidunt sapien. Morbi at nisi dignissim, consectetur tellus vel, malesuada ligula. Sed arcu ex, posuere a mi et, maximus tincidunt mauris. Donec bibendum massa id orci auctor, sed lacinia ligula ornare. Vestibulum metus diam, iaculis sit amet euismod non, blandit eget mi.
+    </h4>
     <h1 class="my-5">
       Personal
     </h1>
