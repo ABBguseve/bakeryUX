b75afcd409ff76ef2432dbc02d2ab06a65a2a4c8
abbgorgar <124578Gd>
2021-05-29 19:13:25 +0200

    info cards

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,37 +1,22 @@
 <template>
   <v-app>
-    <v-toolbar
-      class="px-15"
-      flat
-    >
-      <v-toolbar-title style="font-family: 'Lobster', cursive;">
+    <v-toolbar class="px-15 fixed-bar" flat>
+      <v-toolbar-title style="font-family: 'Lobster', cursive">
         <h2>Bakeriet</h2>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        class="navBtn text-capitalize"
-        plain
-        to="/"
-        color="black"
-      >Start</v-btn>
-      <v-btn
-        class="navBtn text-capitalize"
-        plain
-        to="/varor"
-        color="black"
-      >Varor</v-btn>
-      <v-btn
-        class="navBtn text-capitalize"
-        plain
-        to="/best"
-        color="black"
-      >Beställningar</v-btn>
-      <v-btn
-        class="navBtn text-capitalize"
-        plain
-        to="/omoss"
-        color="black"
-      >Om oss</v-btn>
+      <v-btn class="navBtn text-capitalize" plain to="/" color="black"
+        >Start</v-btn
+      >
+      <v-btn class="navBtn text-capitalize" plain to="/varor" color="black"
+        >Varor</v-btn
+      >
+      <v-btn class="navBtn text-capitalize" plain to="/best" color="black"
+        >Beställningar</v-btn
+      >
+      <v-btn class="navBtn text-capitalize" plain to="/omoss" color="black"
+        >Om oss</v-btn
+      >
       <v-btn
         class="navBtn ml-2"
         rounded
@@ -50,21 +35,14 @@
         clickable
         @click="konto_dialog = !konto_dialog"
       >
-        <img :src="currentUser.avatar">
+        <img :src="currentUser.avatar" />
       </v-avatar>
-
     </v-toolbar>
     <v-main>
       <router-view />
     </v-main>
-    <div
-      v-if="!currentUser"
-      class="text-center"
-    >
-      <v-dialog
-        v-model="login_dialog"
-        width="400"
-      >
+    <div v-if="!currentUser" class="text-center">
+      <v-dialog v-model="login_dialog" width="400">
         <v-card>
           <v-card-title class="navBtn headline grey lighten-2">
             Inloggning
@@ -100,26 +78,20 @@
         </v-card>
       </v-dialog>
     </div>
-    <div
-      v-else
-      class="text-center"
-    >
-      <v-dialog
-        v-model="konto_dialog"
-        width="400"
-      >
+    <div v-else class="text-center">
+      <v-dialog v-model="konto_dialog" width="400">
         <v-card>
           <v-card-title class="navBtn headline grey lighten-2">
             Konto
           </v-card-title>
           <h4 class="px-12 navBtn">Namn</h4>
           <v-spacer></v-spacer>
-          <h4 class="px-12 fontStyle_1">{{currentUser.name}}</h4>
+          <h4 class="px-12 fontStyle_1">{{ currentUser.name }}</h4>
           <v-spacer></v-spacer>
           <v-divider></v-divider>
           <h4 class="px-12 navBtn">E-post</h4>
           <v-spacer></v-spacer>
-          <h4 class="px-12 fontStyle_1">{{currentUser.email}}</h4>
+          <h4 class="px-12 fontStyle_1">{{ currentUser.email }}</h4>
           <v-spacer></v-spacer>
           <v-card-actions>
             <v-spacer></v-spacer>
@@ -142,7 +114,7 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
   name: "App",
@@ -152,44 +124,46 @@ export default {
     konto_dialog: false,
     customers: {},
     currentUser: null,
-    mail: '',
-    password: ''
+    mail: "",
+    password: ""
   }),
-  mounted () {
+  mounted() {
     axios
-      .get('http://localhost:3000/customers')
-      .then(response => (this.customers = response.data))
-    this.currentUser = JSON.parse(localStorage.getItem("Storage_customer"))
+      .get("http://localhost:3000/customers")
+      .then(response => (this.customers = response.data));
+    this.currentUser = JSON.parse(localStorage.getItem("Storage_customer"));
   },
   methods: {
-    logIn () {
-      let customers = this.customers
-      console.log("customers")
-      console.log(this.customers)
-      console.log("currentuser")
-      console.log(this.mail)
-      console.log(this.password)
+    logIn() {
+      let customers = this.customers;
+      console.log("customers");
+      console.log(this.customers);
+      console.log("currentuser");
+      console.log(this.mail);
+      console.log(this.password);
       for (let c in customers) {
         if (this.mail == customers[c].email) {
           if (this.password == customers[c].password) {
-            this.currentUser = customers[c]
-            console.log(customers[c].name)
-            console.log("logged in")
-          }
-          else {
-            console.error("no matching password")
+            this.currentUser = customers[c];
+            console.log(customers[c].name);
+            console.log("logged in");
+          } else {
+            console.error("no matching password");
           }
         }
       }
-      localStorage.setItem('Storage_customer', JSON.stringify(this.currentUser))
-      this.login_dialog = false
+      localStorage.setItem(
+        "Storage_customer",
+        JSON.stringify(this.currentUser)
+      );
+      this.login_dialog = false;
     },
-    logOut () {
-      this.mail = null
-      this.password = null
-      this.currentUser = null
-      this.konto_dialog = false
-      localStorage.clear()
+    logOut() {
+      this.mail = null;
+      this.password = null;
+      this.currentUser = null;
+      this.konto_dialog = false;
+      localStorage.clear();
     }
   }
 };
@@ -201,5 +175,11 @@ export default {
 }
 .fontStyle_1 {
   font-family: "Poppins", sans-serif;
+}
+.fixed-bar {
+  position: sticky;
+  position: -webkit-sticky; /* for Safari */
+  top: 6em;
+  z-index: 2;
 }
 </style>
--- website/src/views/Info.vue
+++ website/src/views/Info.vue
@@ -3,31 +3,20 @@
     class="d-flex flex-column justify-center"
     style="font-family: 'Lobster', cursive; width: 100%; text-align: center"
   >
-    <h1 class="mb-5 ">
-      Information
-    </h1>
-    <h4
-      style="font-family: 'Poppins', sans-serif"
-      class="my-5 px-15"
-    >
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur volutpat orci lectus, id scelerisque tortor fringilla in. Morbi nec tortor tristique, elementum leo eu, commodo ante. Morbi ornare quam sit amet egestas suscipit. Etiam suscipit in erat vel rhoncus. Nam sollicitudin ante neque, ac maximus leo aliquet eget. Donec nec aliquet lacus. Cras nec congue neque, tincidunt aliquet odio. Sed rhoncus quam eu urna finibus, vitae sollicitudin dolor convallis. Proin nulla lorem, accumsan non massa ac, aliquam ornare lorem. Maecenas ac lacus porttitor, euismod mi a, aliquet mi. Fusce et tincidunt sapien. Morbi at nisi dignissim, consectetur tellus vel, malesuada ligula. Sed arcu ex, posuere a mi et, maximus tincidunt mauris. Donec bibendum massa id orci auctor, sed lacinia ligula ornare. Vestibulum metus diam, iaculis sit amet euismod non, blandit eget mi.
+    <h1 class="mb-5">Information</h1>
+    <h4 style="font-family: 'Poppins', sans-serif" class="my-5 px-15">
+      Bakeriet är ett litet företag som säljer olika slags tårtor. Tårtorna som
+      man köper alltid är nybaggade och innehåller bara de bästa ingrädienterna.
+      Våra bagare jobbar för att fylla er dag med glädje och bra smak.
     </h4>
-    <h4
-      style="font-family: 'Poppins', sans-serif"
-      class="my-5 px-15"
-    >
-      Curabitur volutpat orci lectus, id scelerisque tortor fringilla in. Morbi nec tortor tristique, elementum leo eu, commodo ante. Morbi ornare quam sit amet egestas suscipit. Etiam suscipit in erat vel rhoncus. Nam sollicitudin ante neque, ac maximus leo aliquet eget. Donec nec aliquet lacus. Cras nec congue neque, tincidunt aliquet odio. Sed rhoncus quam eu urna finibus, vitae sollicitudin dolor convallis. Proin nulla lorem, accumsan non massa ac, aliquam ornare lorem. Maecenas ac lacus porttitor, euismod mi a, aliquet mi. Fusce et tincidunt sapien. Morbi at nisi dignissim, consectetur tellus vel, malesuada ligula. Sed arcu ex, posuere a mi et, maximus tincidunt mauris. Donec bibendum massa id orci auctor, sed lacinia ligula ornare. Vestibulum metus diam, iaculis sit amet euismod non, blandit eget mi.
-    </h4>
-    <h1 class="my-5">
-      Personal
-    </h1>
+    <h1 class="my-5">Personal</h1>
     <v-spacer></v-spacer>
     <StaffCards> </StaffCards>
   </div>
 </template>
 
 <script>
-import StaffCards from "../components/StaffCards.vue"
+import StaffCards from "../components/StaffCards.vue";
 
 export default {
   components: {
