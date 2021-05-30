18426b0f40c7c2ad7bf1c52fedbbdbd66585864c
abbgorgar <124578Gd>
2021-05-30 22:02:38 +0200

    h

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,22 +1,37 @@
 <template>
   <v-app>
-    <v-toolbar class="px-15 fixed-bar" flat>
+    <v-toolbar
+      class="px-15"
+      flat
+    >
       <v-toolbar-title style="font-family: 'Lobster', cursive">
         <h2>Bakeriet</h2>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn class="navBtn text-capitalize" plain to="/" color="black"
-        >Start</v-btn
-      >
-      <v-btn class="navBtn text-capitalize" plain to="/varor" color="black"
-        >Varor</v-btn
-      >
-      <v-btn class="navBtn text-capitalize" plain to="/best" color="black"
-        >Beställningar</v-btn
-      >
-      <v-btn class="navBtn text-capitalize" plain to="/omoss" color="black"
-        >Om oss</v-btn
-      >
+      <v-btn
+        class="navBtn text-capitalize"
+        plain
+        to="/"
+        color="black"
+      >Start</v-btn>
+      <v-btn
+        class="navBtn text-capitalize"
+        plain
+        to="/varor"
+        color="black"
+      >Varor</v-btn>
+      <v-btn
+        class="navBtn text-capitalize"
+        plain
+        to="/best"
+        color="black"
+      >Beställningar</v-btn>
+      <v-btn
+        class="navBtn text-capitalize"
+        plain
+        to="/omoss"
+        color="black"
+      >Om oss</v-btn>
       <v-btn
         class="navBtn ml-2"
         rounded
@@ -41,8 +56,14 @@
     <v-main>
       <router-view />
     </v-main>
-    <div v-if="!currentUser" class="text-center">
-      <v-dialog v-model="login_dialog" width="400">
+    <div
+      v-if="!currentUser"
+      class="text-center"
+    >
+      <v-dialog
+        v-model="login_dialog"
+        width="400"
+      >
         <v-card>
           <v-card-title class="navBtn headline grey lighten-2">
             Inloggning
@@ -63,6 +84,12 @@
           ></v-text-field>
           <v-card-actions>
             <v-spacer></v-spacer>
+            <h4
+              v-if="logInError"
+              class="fontStyle_1 shake"
+              style="color: red"
+            >Fel E-post eller lösenord</h4>
+            <v-spacer></v-spacer>
             <v-btn
               class="navBtn my-3 mr-8"
               rounded
@@ -78,8 +105,14 @@
         </v-card>
       </v-dialog>
     </div>
-    <div v-else class="text-center">
-      <v-dialog v-model="konto_dialog" width="400">
+    <div
+      v-else
+      class="text-center"
+    >
+      <v-dialog
+        v-model="konto_dialog"
+        width="400"
+      >
         <v-card>
           <v-card-title class="navBtn headline grey lighten-2">
             Konto
@@ -114,7 +147,7 @@
 </template>
 
 <script>
-import axios from "axios";
+import axios from "axios"
 
 export default {
   name: "App",
@@ -125,45 +158,42 @@ export default {
     customers: {},
     currentUser: null,
     mail: "",
-    password: ""
+    password: "",
+    logInError: false
   }),
-  mounted() {
+  mounted () {
     axios
       .get("http://localhost:3000/customers")
-      .then(response => (this.customers = response.data));
-    this.currentUser = JSON.parse(localStorage.getItem("Storage_customer"));
+      .then(response => (this.customers = response.data))
+    this.currentUser = JSON.parse(localStorage.getItem("Storage_customer"))
   },
   methods: {
-    logIn() {
-      let customers = this.customers;
-      console.log("customers");
-      console.log(this.customers);
-      console.log("currentuser");
-      console.log(this.mail);
-      console.log(this.password);
+    logIn () {
+      let customers = this.customers
       for (let c in customers) {
         if (this.mail == customers[c].email) {
           if (this.password == customers[c].password) {
-            this.currentUser = customers[c];
-            console.log(customers[c].name);
-            console.log("logged in");
-          } else {
-            console.error("no matching password");
+            this.currentUser = customers[c]
+            console.log(customers[c].name)
+            console.log("logged in")
+            this.login_dialog = false
+          }
+          else {
+            this.logInError = true
           }
         }
+        else {
+          this.logInError = true
+        }
       }
-      localStorage.setItem(
-        "Storage_customer",
-        JSON.stringify(this.currentUser)
-      );
-      this.login_dialog = false;
+      localStorage.setItem('Storage_customer', JSON.stringify(this.currentUser))
     },
-    logOut() {
-      this.mail = null;
-      this.password = null;
-      this.currentUser = null;
-      this.konto_dialog = false;
-      localStorage.clear();
+    logOut () {
+      this.mail = null
+      this.password = null
+      this.currentUser = null
+      this.konto_dialog = false
+      localStorage.removeItem('Storage_customer')
     }
   }
 };
@@ -176,10 +206,27 @@ export default {
 .fontStyle_1 {
   font-family: "Poppins", sans-serif;
 }
-.fixed-bar {
-  position: sticky;
-  position: -webkit-sticky; /* for Safari */
-  top: 6em;
-  z-index: 2;
+.shake {
+  animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+  transform: translate3d(0, 0, 0);
+}
+@keyframes shake {
+  10%,
+  90% {
+    transform: translate3d(-1px, 0, 0);
+  }
+  20%,
+  80% {
+    transform: translate3d(2px, 0, 0);
+  }
+  30%,
+  50%,
+  70% {
+    transform: translate3d(-4px, 0, 0);
+  }
+  40%,
+  60% {
+    transform: translate3d(4px, 0, 0);
+  }
 }
 </style>
--- website/src/views/Info.vue
+++ website/src/views/Info.vue
@@ -4,7 +4,10 @@
     style="font-family: 'Lobster', cursive; width: 100%; text-align: center"
   >
     <h1 class="mb-5">Information</h1>
-    <h4 style="font-family: 'Poppins', sans-serif" class="my-5 px-15">
+    <h4
+      style="font-family: 'Poppins', sans-serif"
+      class="my-5 px-15"
+    >
       Bakeriet är ett litet företag som säljer olika slags tårtor. Tårtorna som
       man köper alltid är nybaggade och innehåller bara de bästa ingrädienterna.
       Våra bagare jobbar för att fylla er dag med glädje och bra smak.
@@ -16,7 +19,7 @@
 </template>
 
 <script>
-import StaffCards from "../components/StaffCards.vue";
+import StaffCards from "../components/StaffCards.vue"
 
 export default {
   components: {
